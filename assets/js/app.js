bbe206983a8e952be8aad6f1e9dba1a1d2ad1344
Yariqa <masumashikder03@gmail.com>
2021-02-25 23:24:35 +0600

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+// category carousel
+
+$('.category-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive: {
+        0: {
+            items: 2
+        },
+        576: {
+            items: 3
+        },
+        768: {
+            items: 3
+        },
+        992: {
+            items: 4
+        }
+    }
+})
+// product carousel
+
+$('.product-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive: {
+        0: {
+            items: 2
+        },
+        576: {
+            items: 3
+        },
+        768: {
+            items: 3
+        },
+        992: {
+            items: 3
+        }
+    }
+})
