e53f79f2b4012372a9144f7a7c62301720409303
Noore Alam Mahin <50703361+namahin@users.noreply.github.com>
2021-03-03 00:49:38 +0600

    add css extra

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 // category carousel
 $('.category-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
+    loop: true,
+    margin: 25,
+    nav: true,
     responsive: {
         0: {
             items: 2
@@ -13,15 +13,20 @@ $('.category-carousel').owlCarousel({
         992: {
             items: 4
         }
-    }
+    },
+    navText: [
+        '<i class="fal fa-angle-left"></i>',
+        '<i class="fal fa-angle-right"></i>',
+    ],
 })
 
 
 // product carousel
 $('.product-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
+    loop: true,
+    margin: 30,
+    nav: true,
+    dots: false,
     responsive: {
         0: {
             items: 2
@@ -29,5 +34,10 @@ $('.product-carousel').owlCarousel({
         992: {
             items: 3
         }
-    }
+    },
+    navContainer: ".product-card-nav",
+    navText: [
+        '<i class="fal fa-angle-left"></i>',
+        '<i class="fal fa-angle-right"></i>',
+    ],
 })
